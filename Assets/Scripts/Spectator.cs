a56ba97a70d8ac016cf3c4e79498a097cad1f39f
TheSerjio <serggol2006@gmail.com>
2021-11-06 22:56:02 +0200

    spectator

--- Assets/Scripts/Spectator.cs
+++ Assets/Scripts/Spectator.cs
@@ -5,14 +5,16 @@ public sealed class Spectator : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
-    public float scrollSpeed;
     public float zRotationSpeed;
+    public float mouseSpeed;
 
     public void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, Input.GetAxis("Mouse ScrollWheel") * scrollSpeed, Space.Self);
-        if (Input.GetMouseButton(2))
+        transform.Translate(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed, Space.Self);
+        if (Input.GetMouseButton(0))
             transform.Rotate(Input.GetAxis("Mouse Y") * rotationSpeed, -Input.GetAxis("Mouse X") * rotationSpeed, 0, Space.Self);
+        if (Input.GetMouseButton(1))
+            transform.Translate(-Input.GetAxis("Mouse X") * mouseSpeed, -Input.GetAxis("Mouse Y") * mouseSpeed, 0, Space.Self);
         if (Input.GetKey(KeyCode.Q))
             transform.Rotate(0, 0, Time.deltaTime * zRotationSpeed, Space.Self);
         if (Input.GetKey(KeyCode.E))
