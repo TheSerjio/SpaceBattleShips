8db5e51d62e898654f19b16575d4f270311081aa
SoSad24 <sergiusp24@gmail.com>
2021-11-22 21:09:19 +0200

    +

--- Assets/Scripts/DustNearPlayer.cs
+++ Assets/Scripts/DustNearPlayer.cs
@@ -18,7 +18,7 @@ public class DustNearPlayer : MonoBehaviour
     {
         ve.SetVector3("grav", transform.InverseTransformDirection(-rb.velocity));
         var q = rb.velocity.magnitude / (rb.velocity.magnitude + 1);
-        ve.SetFloat("X scale", q);
+        ve.SetFloat("X scale", q/2);
         ve.SetFloat("Y scale", q * 10);
         ve.SetFloat("capacity", Mathf.Clamp(rb.velocity.magnitude, 0, 300));
         if (!pm)
