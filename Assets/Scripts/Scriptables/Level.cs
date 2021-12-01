a7b889d0a7d295dff8d30bac970184eb35b4186d
TheSerjio <serggol2006@gmail.com>
2021-12-01 20:38:04 +0200

    levelz

---
+++ Assets/Scripts/Scriptables/Level.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "Object/Level")]
+public class Level : ValidableScriptableObject
+{
+#if UNITY_EDITOR
+    public UnityEditor.SceneAsset scene;
+#endif
+    public ShipData[] ships;
+    public int BuildingIndex;
+
+    public override IEnumerable<Warning> Validate()
+    {
+#if UNITY_EDITOR
+        BuildingIndex = SceneUtility.GetBuildIndexByScenePath(UnityEditor.AssetDatabase.GetAssetPath(scene));
+        UnityEditor.EditorUtility.SetDirty(this);
+#endif
+        if (!scene)
+            yield return Warn(Level.Error, "no scene");
+    }
+}
