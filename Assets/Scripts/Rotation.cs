75976906a52cfdc0af39bcda4354a119f510d026
TheSerjio <serggol2006@gmail.com>
2021-12-08 21:42:23 +0200

    earth random rotation

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -6,6 +6,11 @@ public sealed class Rotation : MonoBehaviour
     public Vector3 rotation;
     public Space how;
 
+    public void Start()
+    {
+        transform.Rotate(rotation * 1000 * Random.value, how);
+    }
+
     public void Update()
     {
         transform.Rotate(rotation * Time.deltaTime, how);
