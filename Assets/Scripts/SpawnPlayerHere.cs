dd91ed41e52b7c0eaa79edf95ef6844ba6059f18
SoSad24 <sergiusp24@gmail.com>
2021-12-08 21:50:37 +0200

    nb

--- Assets/Scripts/SpawnPlayerHere.cs
+++ Assets/Scripts/SpawnPlayerHere.cs
@@ -6,6 +6,6 @@ public class SpawnPlayerHere : SINGLETON<SpawnPlayerHere>
     public void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(Ttransform.position, Ttransform.position + (Ttransform.forward * 100));
+        Gizmos.DrawLine(Ttransform.position, Ttransform.position + (Ttransform.forward * 1000));
     }
 }
