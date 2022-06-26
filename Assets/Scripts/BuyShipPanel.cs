f4f5182ce8303afbda5be0a5e1d7a7a226ccf96a
TheSerjio <serggol2006@gmail.com>
2022-06-26 15:01:47 +0300

    e

---
+++ Assets/Scripts/BuyShipPanel.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyShipPanel : Script
+{
+    public Image icon;
+    public ShipData ship;
+    public Text text;
+    public Slider slider;
+
+    protected override void OnAwake() { }
+
+    public void ShipSelect(ShipData data)
+    {
+        ship = data;
+        icon.sprite = data ? data.Preview : null;
+    }
+
+    public void OnEnable()
+    {
+        ShipSelect(null);
+    }
+
+    public void Buy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (ship)
+            text.text = $"{ship.Name} x{slider.value}\n{slider.value * ship.cost}$";
+        else
+            text.text = "Select ship";
+    }
+}
